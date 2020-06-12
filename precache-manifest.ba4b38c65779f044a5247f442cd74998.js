d005fbb7c9c325caec6863c46e3d197f7fb1b5dd
AIpodvolotskii <32352980+AIpodvolotskii@users.noreply.github.com>
2020-06-12 20:57:50 +0300

    Updates

---
+++ precache-manifest.ba4b38c65779f044a5247f442cd74998.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5f0dae708a439ee0f37a56921073c9",
+    "url": "/lego-room-ru/index.html"
+  },
+  {
+    "revision": "d4be888ad6fc87991e86",
+    "url": "/lego-room-ru/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "bbb205601590afa815f9",
+    "url": "/lego-room-ru/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d4be888ad6fc87991e86",
+    "url": "/lego-room-ru/static/js/2.d8794e14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lego-room-ru/static/js/2.d8794e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb205601590afa815f9",
+    "url": "/lego-room-ru/static/js/main.035db97d.chunk.js"
+  },
+  {
+    "revision": "1230f20f57a5c93212fb",
+    "url": "/lego-room-ru/static/js/runtime-main.fe20259e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/lego-room-ru/static/media/persik.4e1ec840.png"
+  }
+]);
